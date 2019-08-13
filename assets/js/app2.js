99f228503395a53796194cd570a18cb97583433e
sartaal <49984870+sartaal@users.noreply.github.com>
2019-08-13 12:58:12 +0600

    1213

---
+++ assets/js/app2.js
@@ -0,0 +1,25 @@
+
+
+ 
+
+
+  $("#datepicker1" ).datepicker({
+      showWeek: true,
+      firstDay: 1
+  });
+
+$("#datepicker2" ).datepicker({
+      showWeek: true,
+      firstDay: 1
+  });
+
+
+
+ 
+
+
+
+
+
+
+ 

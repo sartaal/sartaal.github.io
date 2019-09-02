b3effe3f7c934d624cc398f94abd7f3eef9f7c08
sartaal <49984870+sartaal@users.noreply.github.com>
2019-09-02 22:39:09 +0600

    23123123
    123123

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+$.noConflict();
+ jQuery('.slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    dots: false,
+    аccessibility: false,
+    arrows: false,
+    centerMode: true,
+    variableWidth: true,
+  });
+ 
+ lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  });
+
+ 
+
+
+ 
+ 
+
+
+
+
+
+
+ 
